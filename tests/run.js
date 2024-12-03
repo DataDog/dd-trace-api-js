b87b5d761bed3ad6f85a210cd340e415ae30424b
Bryan English <bryan.english@datadoghq.com>
2024-12-02 22:31:01 -0500

    drop support for versions lower than 18, since dd-trace-js doesn't support them

--- tests/run.js
+++ tests/run.js
@@ -4,12 +4,12 @@ let testCommand = 'node --test'
 
 if (MAJOR >= 22 || (MAJOR === 20 && MINOR >= 1) || (MAJOR === 18 && MINOR >= 17)) {
   testCommand = 'node --experimental-test-coverage --test'
-} else if (MAJOR >= 20 || (MAJOR === 18 && MINOR >= 1) || (MAJOR === 16 && MINOR >= 17)) {
+} else if (MAJOR >= 20 || (MAJOR === 18 && MINOR >= 1)) {
   testCommand = 'node --test'
-} else if (MAJOR >= 14) {
+} else if (MAJOR >= 18) {
   testCommand = 'node_modules/.bin/node--test'
 } else {
-  console.error('Node.js version 14 or higher is required.')
+  console.error('Node.js version 18 or higher is required.')
   process.exit(1)
 }
 
